41775c5564ba4f86dbbf3daac543e50fd6e6ca8e
Sylwia <sylwia.szusz@gmail.com>
2026-06-02 00:30:20 +0200

    final commit (tailwind, usuniecie rzeczy z poprzedniego zadania)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import "./style.css";
 
 const URL = "https://flrtxyteviumfhkdtrvx.supabase.co/rest/v1/article?select=*";
 const KEY = "sb_publishable_FKoQFi6-aJGI3dlUWTyANg_sE-u3lNG";
@@ -17,11 +18,11 @@ function pokazArtykuly() {
       const app = document.querySelector("#app");
       artykuly.forEach(artykul => {
         app.innerHTML += `
-          <article>
-            <h2>${artykul.title}</h2>
-            <p>${artykul.subtitle}</p>
-            <p>Autor: ${artykul.author}</p>
-            <p>Data: ${dayjs(artykul.created_at).format("DD-MM-YYYY")}</p>
+          <article class="bg-white border border-gray-200 rounded-xl shadow-md p-6 mb-5">
+            <h2 class="text-2xl font-bold">${artykul.title}</h2>
+            <p class="text-gray-500 mb-2">${artykul.subtitle}</p>
+            <p class="text-sm text-gray-400">Autor: ${artykul.author}</p>
+            <p class="text-sm text-gray-400 mb-3">Data: ${dayjs(artykul.created_at).format("DD-MM-YYYY")}</p>
             <p>${artykul.content}</p>
           </article>
         `;
@@ -38,10 +39,10 @@ formularz.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nowyArtykul = {
-    title:    document.querySelector("#title").value,
+    title: document.querySelector("#title").value,
     subtitle: document.querySelector("#subtitle").value,
-    author:   document.querySelector("#author").value,
-    content:  document.querySelector("#content").value,
+    author: document.querySelector("#author").value,
+    content: document.querySelector("#content").value,
     created_at: document.querySelector("#created_at").value,
   };
 
@@ -58,5 +59,7 @@ formularz.addEventListener("submit", (event) => {
     .then(res => res.json())
     .then(odpowiedz => {
       console.log("Dodano:", odpowiedz);
+      formularz.reset();
+      pokazArtykuly();
     });
 });
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/sem-2-tw-laby/',
+  base: '/sem-2-tw-laby06/',
   plugins: [tailwindcss()],
 })
